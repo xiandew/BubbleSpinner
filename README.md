ff4c67d5fedd7d84fcafad8e2e2e622be247b907
Xiande Wen <visiande1998@gmail.com>
2018-10-14 18:56:51 +1100

    Test

--- README.md
+++
@@ -1,2 +0,0 @@
-# HitBeans
-A simple WeChat game. Inspired by the flash version on 4399 and reproduce that in JS.
---
+++ game.js
@@ -0,0 +1,15 @@
+wx.onTouchStart(function (e) {
+  console.log(e.touches)
+})
+
+wx.onTouchMove(function (e) {
+  console.log(e.touches)
+})
+
+wx.onTouchEnd(function (e) {
+  console.log(e.touches)
+})
+
+wx.onTouchCancel(function (e) {
+  console.log(e.touches)
+})
