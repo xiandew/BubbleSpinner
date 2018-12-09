77788665bba541421842e3eaac745faff48a5c6c
Xiande Wen <visiande1998@gmail.com>
2018-12-09 13:16:22 +0800

    2.4.3 分数从周一更新

--- js/myOpenDataContext/utilities/getCurrentWeek.js
+++ js/myOpenDataContext/utilities/getCurrentWeek.js
@@ -1,11 +1,19 @@
 module.exports = function() {
         let today = new Date();
-        let newYearsDay = new Date(today.getFullYear(), 0, 1);
-        let days = Math.round(
-                (today.valueOf() - newYearsDay.valueOf()) / 86400000
+        let newYearsDate = new Date(today.getFullYear(), 0, 1);
+
+        // start from Monday
+        let offsetDays = 1;
+        let newYearsDay = newYearsDate.getDay();
+        if (newYearsDay != 0) {
+                offsetDays += (7 - newYearsDay);
+        }
+
+        let firstMonday = new Date(today.getFullYear(), 0, 1 + offsetDays);
+        let days = Math.ceil(
+                (today.valueOf() - firstMonday.valueOf()) / 86400000
         );
 
-        return Math.ceil(
-                (days + newYearsDay.getDay()) / 7
-        );
+        // count the first full week as 1
+        return Math.ceil(days / 7) + 1;
 };
