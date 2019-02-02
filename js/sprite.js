f295ff23903335f3109d53b65e40b4a052c3eee4
Xiande Wen <visiande1998@gmail.com>
2019-02-02 18:30:22 +0800

    minor

--- js/sprite.js
+++ js/sprite.js
@@ -4,15 +4,22 @@ let newImage = require('./utilities/newImage');
 // Top level `abstract` class for the game
 export default class Sprite {
         constructor(imgSrc = "", width = 0, height = 0, x = 0, y = 0) {
-                this.img    = newImage(imgSrc);
-                this.width  = width;
+                this.img = newImage(imgSrc);
+                this.width = width;
                 this.height = height;
+
                 this.x = x;
                 this.y = y;
+
+                this.visiable = true;
         }
 
         // cannot use 'onload' method for drawing image during 'reuireAnimationFrame'
         render() {
+                if (!this.visiable) {
+                        return;
+                }
+
                 // draw the image from the center at (x, y)
                 ctx.drawImage(
                         this.img,
@@ -24,14 +31,14 @@ export default class Sprite {
         }
 
         isCollideWith(other) {
-                let otherLeft   = other.x - other.width  / 2;
-                let otherRight  = other.x + other.width  / 2;
-                let otherTop    = other.y - other.height / 2;
+                let otherLeft = other.x - other.width / 2;
+                let otherRight = other.x + other.width / 2;
+                let otherTop = other.y - other.height / 2;
                 let otherBottom = other.y + other.height / 2;
 
-                return  otherLeft   < this.x + this.width  / 2 &&
-                        otherRight  > this.x - this.width  / 2 &&
-                        otherTop    < this.y + this.height / 2 &&
+                return otherLeft < this.x + this.width / 2 &&
+                        otherRight > this.x - this.width / 2 &&
+                        otherTop < this.y + this.height / 2 &&
                         otherBottom > this.y - this.height / 2;
         }
 }
