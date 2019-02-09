f836d4be04ae7fd824ba4ba22e9fac66572eee21
Xiande Wen <visiande1998@gmail.com>
2019-02-09 13:47:38 +0800

    Minor

--- js/ball.js
+++ js/ball.js
@@ -122,21 +122,4 @@ export default class Ball extends Sprite {
                 this.speedX = theSpeed * Math.cos(ha);
                 this.speedY = theSpeed * Math.sin(ha);
         }
-
-        // draw a circle shape instead of image. Not display well on the phone
-        /*
-	render(ctx) {
-		ctx.beginPath()
-                ctx.fillStyle = this.colour
-                ctx.arc(this.getX(), this.getY(), this.size / 2, 0, 2 * Math.PI)
-                ctx.fill()
-                ctx.closePath()
-
-                ctx.beginPath()
-                ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
-                ctx.arc(this.getX() - this.size / 6, this.getY() + this.size / 6, this.size / 6, 0, 2 * Math.PI)
-                ctx.fill()
-                ctx.closePath()
-        }
-	*/
 }
