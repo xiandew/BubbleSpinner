392fba5948d1cec8f86e253872067aaa282e3ddc
Xiande Wen <visiande1998@gmail.com>
2020-07-18 17:41:28 +1000

    Update assets loader

--- src/data/AssetsLoader.js
+++ src/data/AssetsLoader.js
@@ -3,22 +3,20 @@ import Assets from './Assets.js';
 export default class AssetsLoader {
     constructor(assets = Assets) {
         this.assets = new Map(assets);
-        for (let [key, value] of this.assets) {
-            const image = wx.createImage();
-            image.src = value;
-            this.assets.set(key, image);
-        }
     }
 
     onLoaded(callback) {
         let loadedCount = 0;
-        for (let asset of this.assets.values()) {
-            asset.onload = () => {
+        for (let [key, value] of this.assets) {
+            const image = wx.createImage();
+            image.onload = () => {
                 loadedCount++;
                 if (loadedCount >= this.assets.size) {
                     callback(this.assets);
                 }
-            }
+            };
+            image.src = value;
+            this.assets.set(key, image);
         }
     }
 
