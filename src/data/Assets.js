c22580e5abc8444345b1e0bdbc8a2a5cd3b46983
Xiande Wen <visiande1998@gmail.com>
2020-08-11 18:56:02 +1000

    Minor update of reformatting code

--- src/data/Assets.js
+++ src/data/Assets.js
@@ -1,33 +1,33 @@
 // Store as two-dimension array for new Map()
 const Assets = [
     // Usage: MainMenu
-    ["branding",  "assets/img/branding.png" ],
-    ["logo",      "assets/img/logo-2d.png"  ],
+    ["branding", "assets/img/branding.png"],
+    ["logo", "assets/img/logo-2d.png"],
     ["start-btn", "assets/img/start-btn.png"],
-    ["rank-btn",  "assets/img/rank-btn.png" ],
+    ["rank-btn", "assets/img/rank-btn.png"],
 
     // Usage: MainScene
-    ["pivot",         "assets/img/pivot.png"        ],
-    ["blue-bubble",   "assets/img/blue-bubble.png"  ],
-    ["cyan-bubble",   "assets/img/cyan-bubble.png"  ],
-    ["red-bubble",    "assets/img/red-bubble.png"   ],
+    ["pivot", "assets/img/pivot.png"],
+    ["blue-bubble", "assets/img/blue-bubble.png"],
+    ["cyan-bubble", "assets/img/cyan-bubble.png"],
+    ["red-bubble", "assets/img/red-bubble.png"],
     ["yellow-bubble", "assets/img/yellow-bubble.png"],
-    ["pink-bubble",   "assets/img/pink-bubble.png"  ],
-    ["green-bubble",  "assets/img/green-bubble.png" ],
-    ["gray-bubble",   "assets/img/gray-bubble.png"  ],
+    ["pink-bubble", "assets/img/pink-bubble.png"],
+    ["green-bubble", "assets/img/green-bubble.png"],
+    ["gray-bubble", "assets/img/gray-bubble.png"],
 
     // Usage: GameEnded
     ["restart-btn", "assets/img/restart-btn.png"],
 
     // Usage: RankScene (onscreen canvas)
     ["group-rank-btn", "assets/img/group-rank-btn.png"],
-    ["return-btn",     "assets/img/return.png"        ],
+    ["return-btn", "assets/img/return.png"],
 
-	// Usage: wx.onShareAppMessage()
-	["share-img", "assets/img/share.png"],
+    // Usage: wx.onShareAppMessage()
+    ["share-img", "assets/img/share.png"],
 
     // Bitmap font. Usage: MainScene
-	["DustyPro", "assets/bmfonts/DustyPro.png"],
+    ["DustyPro", "assets/bmfonts/DustyPro.png"],
 ];
 
 export default Assets;
