513ec4ad490fe46a7a76f5701cdfffe0254d3363
Xiande Wen <visiande1998@gmail.com>
2021-01-10 13:07:10 +1100

    feat: create browser view for desktop testing

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+
+    entry: {
+        // "bubble-spinner": path.join(__dirname, './game'),
+        "bubble-spinner.min": path.join(__dirname, './game')
+    },
+
+    module: {
+        rules: [{
+            test: /libs/,
+            use: 'null-loader'
+        }, {
+            test: /\.(j|t)sx?$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: "babel-loader",
+                options: {
+                    presets: ['babel-preset-expo'],
+                    env: {
+                        development: {
+                            plugins: [
+                                '@babel/plugin-transform-react-jsx-source',
+                            ],
+                        },
+                    },
+                }
+            }]
+        }],
+    },
+
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: '[name].js'
+    },
+
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                include: /\.min\.js$/,
+                parallel: true,
+                sourceMap: false,
+                uglifyOptions: {
+                    compress: true,
+                    ie8: false,
+                    ecma: 5,
+                    output: { comments: false },
+                    warnings: false
+                },
+                warningsFilter: () => false
+            })
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+}
